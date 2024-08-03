80df51aedee9922c767b56142de8878388cd05db
Anchit Sinha <anchit1909@gmail.com>
2024-08-03 13:16:55 +0530

    feat: add a constants file

---
+++ src/utils/constants.js
@@ -0,0 +1,11 @@
+export const INPUT_OPTIONS = [
+  { text: "Text", value: "text" },
+  { text: "File", value: "file" },
+];
+
+export const NODE_TYPES = {
+  input: "input",
+  output: "output",
+  text: "text",
+  llm: "llm",
+};
